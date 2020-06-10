042d4a21ede6fbe4080a971503bd7fb6d701b507
kiquetal <kiquetal@gmail.com>
2020-06-09 22:49:45 -0400

    adding models and db

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@
 // This is the main entry point of our application
  const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
-
+require("dotenv").config();
+const db=require("./db");
+const models = require("./models");
 // Run the server on a port specified in our .env file or port 4000
 const port = process.env.PORT || 4000;
-
+const DB_HOST = process.env.DB_HOST;
 let notes = [
 	{ id:'1',content:'This is a note',author:'Scott'},
 	{ id:'2',content:'This is another note',author:'Scott2'},
@@ -39,29 +41,25 @@ type Mutation {
 const resolvers = {
   Query: {
     hello: () => 'Hello world!',
-    notes:() => notes ,
-    note:(parent,args)=> {
+    notes:async () => await models.Note.find() ,
+    note:async (parent,args)=> {
 
-    return notes.find(note=>note.id === args.id);
+    return await models.Note.findById(args.id);
     }
   },
  Mutation:{
-	newNote:(parent,args)=>{
-
-		let noteValue={
-		id:String(notes.length+1),
-		content:args.content,
-		author:'Scott'
-
-		}
-		notes.push(noteValue);
-		return noteValue;
+	newNote:async (parent,args)=>{
+	return await models.Note.create({content:args.content,author:"Adam Scott"})
+		
 	}
 
  }
 };
 
 const app = express();
+
+db.connect(DB_HOST);
+
 
 // Apollo Server setup
 const server = new ApolloServer({ typeDefs, resolvers });
